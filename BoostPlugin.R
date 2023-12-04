4abd8a3e17d8180009e90978b2eb48b16a63e8c1
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-12-04 17:13:59 -0500

    Initial

---
+++ BoostPlugin.R
@@ -0,0 +1,67 @@
+library(AppliedPredictiveModeling)
+transparentTheme(trans = .4)
+library(caret)
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
+
+input <- function(inputfile) {
+	pfix = prefix()
+  parameters <<- read.table(inputfile, as.is=T);
+  rownames(parameters) <<- parameters[,1];
+   # Need to get the three files
+   csvfile <<- paste(pfix, parameters["csvfile", 2], sep="/")
+
+   training <<- read.csv(csvfile)
+   fitControl <<- readRDS(paste(pfix, parameters["fitControl", 2], sep="/"))
+
+   myType <<- "Regular"
+  if ("type" %in% rownames(parameters)) {
+  myType  <<- as.integer(parameters["type", 2])
+  }
+
+}
+
+run <- function() {}
+
+output <- function(outputfile) {
+set.seed(825)
+if (myType == "GAM") {
+result <- train(Class ~ ., data = training, 
+                 method = "gamboost", 
+                 trControl = fitControl)
+}
+else if (myType == "GLM") {
+result <- train(Class ~ ., data = training, 
+                 method = "glmboost", 
+                 trControl = fitControl)
+}
+else if (myType == "LM") {
+result <- train(Class ~ ., data = training, 
+                 method = "bstLm", 
+                 trControl = fitControl)
+}
+else if (myType == "Logit") {
+result <- train(Class ~ ., data = training, 
+                 method = "LogitBoost", 
+                 trControl = fitControl)
+}
+else if (myType == "SS") {
+result <- train(Class ~ ., data = training, 
+                 method = "bstSm", 
+                 trControl = fitControl)
+}
+else if (myType == "Black") {
+result <- train(Class ~ ., data = training, 
+                 method = "blackboost", 
+                 trControl = fitControl)
+}
+else {
+result <- train(Class ~ ., data = training, 
+                 method = "bstTree", 
+                 trControl = fitControl)
+}
+print(result)
+saveRDS(result, outputfile)
+}
